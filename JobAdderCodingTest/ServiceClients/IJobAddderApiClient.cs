133866fbb07793653e1f52d841a8afcea52acaf1
DESKTOP-QMNCA10\Admin <angelico_cruz@yahoo.com>
2020-11-23 01:33:14 +1100

    Added http client and automapper

---
+++ JobAdderCodingTest/ServiceClients/IJobAddderApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobAdderCodingTest.ServiceClients
+{
+	public interface IJobAddderApiClient
+	{
+		Task<List<Candidate>> GetCandidates();
+		Task<List<Job>> GetJobs();
+	}
+}
---
+++ JobAdderCodingTest/ServiceClients/JobAddderApiClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace JobAdderCodingTest.ServiceClients
+{
+	public class JobAddderApiClient : IJobAddderApiClient
+	{
+		private readonly string _baseAddress;
+		private readonly HttpClient _client;
+		private readonly IMapper _mapper;
+
+		public JobAddderApiClient(IConfiguration configuration, HttpClient client, IMapper mapper)
+		{
+			_client = client;
+			_mapper = mapper;
+			_baseAddress = configuration.GetSection("JobAdderApi").GetValue<string>("BaseUrl");
+		}
+
+		public async Task<List<Candidate>> GetCandidates()
+		{
+			var apiCallTask = _client.GetStreamAsync(new Uri($"{_baseAddress}/candidates")).ConfigureAwait(false);
+			var candidateResponse = await JsonSerializer.DeserializeAsync<List<CandidateResponse>>(await apiCallTask);
+			return candidateResponse.Select(x => _mapper.Map<Candidate>(x)).ToList();
+		}
+
+		public async Task<List<Job>> GetJobs()
+		{
+			var apiCallTask = _client.GetStreamAsync(new Uri($"{_baseAddress}/jobs")).ConfigureAwait(false);
+			var jobResponse = await JsonSerializer.DeserializeAsync<List<JobResponse>>(await apiCallTask);
+			return jobResponse.Select(x => _mapper.Map<Job>(x)).ToList();
+		}
+	}
+}

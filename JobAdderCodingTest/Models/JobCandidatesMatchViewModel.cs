b3ef36705b252f9a36f996d8935f07735201c920
DESKTOP-QMNCA10\Admin <angelico_cruz@yahoo.com>
2020-11-23 01:34:41 +1100

    Added view models

---
+++ JobAdderCodingTest/Models/JobCandidatesMatchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobAdderCodingTest.Models
+{
+	public class JobCandidatesMatchViewModel
+	{
+		public int JobId { get; set; }
+		public string Name { get; set; }
+		public string Company { get; set; }
+		public string Skills { get; set; }
+		public IEnumerable<CandidateViewModel> Candidates { get; set; }
+	}
+}
